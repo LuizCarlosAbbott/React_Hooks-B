dc6ed3b18f05339bb0a463fb8ab9c8100c9d01db
@LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-04-06 00:01:39 -0300

    useState example

---
+++ react-hooks/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useForm } from "./useForm";
+
+const App = () => {
+  const [values, handleChange] = useForm({ email: "", password: "" });
+
+  return (
+    <div>
+      <input name="email" value={values.email} onChange={handleChange} />
+      <input
+        name="password"
+        type="password"
+        value={values.password}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+export default App;
